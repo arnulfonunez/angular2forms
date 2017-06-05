bea727a78f521aacaef41702d334b4cf84a73fb3
nuneza <arnulfo.nunez@stifel.com>
2017-06-05 11:12:46 -0400

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from '../models/employee.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+ 
+  public languages:string[] = ['English', 'Spanish', 'Other'];
+  public employee:Employee = new Employee('','',true,'W2','default');
+  public hasPrimaryLanguageError:boolean = false;
+
+
   constructor() { }
 
   ngOnInit() {
+
   }
+
+public validatePrimaryLanguage(value:string):void{
+  this.hasPrimaryLanguageError = false;
+
+  if(value === 'default')
+  {
+    this.hasPrimaryLanguageError = true;
+  }
+
+}
+
+
 
 }
